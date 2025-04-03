5a8d097dff18888ccd76c14c8c870808308c9696
pedrincaneladefogo <pedro.silva.camargo@escola.pr.gov.br>
2025-04-02 21:01:30 -0300

    Add files via upload

---
+++ pedro3-E-main/pedro3-E-main/pedro/JS script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded',Function()
+const botaoDeAcessibilidade=document.getElementById('botso-acessibilidade');
+const opcoesDeAcessibilidade=document.getElementById('opcoes-acessibilidade');
+
+botaoDeAcessibilidade.addEventListener('click',function(){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle(apresenta-lista);
+
+    const botaoSelecionad=botaoDeAcessibilidade.getAttribute('aria-expanded')===
+
+})
+
+
+
+
+
+

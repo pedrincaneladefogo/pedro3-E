4845c9278f8df8b503329f438e0d00edd5efe5c9
pedrincaneladefogo <pedro.silva.camargo@escola.pr.gov.br>
2025-05-21 20:58:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const botoes =document.querySelectorAll(".botao");
+const textos =document.querySelectorAll(".aba-conteudo");
+
+for (let  i=0; i <botoes.length;++){
+
+    botoes[i].onlcick=function{
+
+        for (let j=0; j < botoes.length; j++){
+            botoes[j].classList
+        }
+    }
+}
